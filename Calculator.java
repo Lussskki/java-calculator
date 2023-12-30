f7b39f429ba49e41fdd704c8adb9f523f34d0f6a
Luka Guledani <lukaguledani123@gmail.com>
2023-12-30 19:54:40 +0400

    Feat: Simple calculator

---
+++ Calculator.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+
+        while (true) {
+            System.out.println("Choose operation:");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+
+            int choice = scanner.nextInt();
+
+            if (choice == 5) {
+                System.out.println("Exiting the calculator. Goodbye!");
+                break;
+            }
+
+            System.out.print("Enter first number: ");
+            int num1 = scanner.nextInt();
+
+            System.out.print("Enter second number: ");
+            int num2 = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Result: " + add(num1, num2));
+                    break;
+                case 2:
+                    System.out.println("Result: " + subtract(num1, num2));
+                    break;
+                case 3:
+                    System.out.println("Result: " + multiply(num1, num2));
+                    break;
+                case 4:
+                    if (num2 != 0) {
+                        System.out.println("Result: " + divide(num1, num2));
+                    } else {
+                        System.out.println("Error: Cannot divide by zero.");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
+    }
+
+    private static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    private static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    private static int divide(int a, int b) {
+        return a / b;
+    }
+}
